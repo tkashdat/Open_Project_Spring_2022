bc188ebbcaeadc8a745123c2ed2ea8ffdcd5f03f
djlittle <42420410+djlittle@users.noreply.github.com>
2022-01-13 09:29:12 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,93 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Name:         main.py
+# Purpose:      Open Project Fall - 2022: Predict mortality with medical data
+#               CodaLab competition: https://competitions.codalab.org/competitions/27605#learn_the_details
+#
+# Author(s):    David Little
+#
+# Created:      12/17/2021
+# Updated:      12/17/2021
+# Update Comment(s):
+#
+# TO DO:
+#
+# FUTURE WORK:
+#
+# BUGS TO FIX:
+#
+# ----------------------------------------------------------------------------------------------------------------------
+import numpy as np
+import pandas as pd
+
+# _______________________ Import __________________________________________________________
+all_data = pd.read_csv('mimic_synthetic_train.csv', delimiter=' ', header=None)
+col_names = pd.read_csv('mimic_synthetic_feat.csv', delimiter=' ', header=None)
+all_data = all_data.iloc[:,1:]
+all_data.set_axis(col_names, axis=1, inplace=True)
+
+labels = pd.read_csv('mimic_synthetic_train_labels.csv', delimiter=' ', header=None)
+all_data['DIED'] = labels
+
+# _______________________ Identify constant columns_________________________________
+non_dups = []
+for column in all_data:
+    if all_data[column].astype(str).min() == all_data[column].astype(str).max():
+        non_dups.append(column)
+
+all_data.drop(non_dups, axis=1, inplace=True)
+
+# _______________________ Drop non-informative _________________________________
+all_data = all_data.iloc[:,4:]
+
+# _______________________ Just the categorical _________________________________
+
+#____________________________ One-hot encoding_________________________________
+
+
+#_______________________test_train split_________________________________________
+
+
+#____________________________ Upsampling _________________________________
+
+
+
+# _______________________ Modeling _________________________________
+
+import xgboost as xgb
+
+dtrain = xgb.DMatrix(all_data_train.iloc[:,:-1], enable_categorical=True, label=all_data_train['DIED'])
+
+print("Booster parameters")
+param = {'max_depth': 10, 'eta': 0.2, 'objective': 'binary:hinge'}
+param['nthread'] = 4
+param['eval_metric'] = 'auc'
+print("train xgboost")
+num_round = 20
+cls = xgb.train(param, dtrain, num_round)
+#bst.save_model('xgboost.model')
+dtest = xgb.DMatrix(X_test)
+
+preds = cls.predict(dtest)
+
+from sklearn.metrics import f1_score
+
+acc = f1_score(y_test, preds)
+print(acc)
+
+from sklearn.metrics import accuracy_score
+
+acc = accuracy_score(y_test, preds)
+print(acc)
+
+
+from sklearn.metrics import balanced_accuracy_score
+acc = balanced_accuracy_score(y_test, preds)
+print(acc)
+
+
+xgb.plot_importance(cls, max_num_features=80,grid=False)
+
+#______________________________________ Predict test case & save _________________________________________
+
+
+
